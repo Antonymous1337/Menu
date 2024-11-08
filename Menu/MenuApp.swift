c7901ced2daa34aa25a4c0c69ef3065d410a04b5
Antonymous1337 <holshouserantony@yahoo.com>
2024-11-08 15:52:41 -0800

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Antony Holshouser on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
