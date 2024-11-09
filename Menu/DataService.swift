f44c053c8ee83944d6be7db41fa9985462814292
Antonymous1337 <holshouserantony@yahoo.com>
2024-11-08 17:52:21 -0800

    Finished Project

---
+++ Menu/DataService.swift
@@ -0,0 +1,8 @@
+//
+//  DataService.swift
+//  Menu
+//
+//  Created by Antony Holshouser on 11/8/24.
+//
+
+import Foundation
---
+++ Menu/MenuItem.swift
@@ -0,0 +1,8 @@
+//
+//  MenuItem.swift
+//  Menu
+//
+//  Created by Antony Holshouser on 11/8/24.
+//
+
+import Foundation
